c803e8a1441ae9dc97c00c136ef1fd791c34218d
diverds1992 <78227663+diverds1992@users.noreply.github.com>
2022-02-01 16:15:53 +0100

    Add files via upload

---
+++ Extension.php
@@ -0,0 +1,74 @@
+<?php 
+
+namespace Thoughtco\OrderEditor;
+
+use Event;
+use Admin\Models\Orders_model;
+use Admin\Widgets\Form;
+use Admin\Widgets\Toolbar;
+use System\Classes\BaseExtension;
+
+/**
+ * Order Editor Extension Information File
+**/
+class Extension extends BaseExtension
+{
+    public function boot()
+    {
+        // extend fields on orders model
+        Event::listen('admin.form.extendFieldsBefore', function (Form $form) {
+            if ($form->model instanceof Orders_model) {
+			$form->tabs['fields']['order_time_is_asap'] = [
+				'tab' => 'lang:thoughtco.ordereditor::default.edit_time_date',
+				'label' => 'lang:admin::lang.orders.label_time_is_asap',
+				'type' => 'switch',
+				'on' => 'lang:admin::lang.text_yes',
+				'off' => 'lang:admin::lang.text_no',
+				'disabled' => FALSE,
+				'context' => ['edit', 'preview'],
+			];	
+				
+			$form->tabs['fields']['order_time'] = [
+				'tab' => 'lang:thoughtco.ordereditor::default.edit_time_date',
+				'label' => 'lang:admin::lang.orders.label_order_time',
+				'type' => 'datepicker',
+				'disabled' => FALSE,
+				'mode' => 'time',
+				'span' => 'left',
+				'cssClass' => 'flex-width',
+			];
+				
+			$form->tabs['fields']['order_date'] = [
+				'tab' => 'lang:thoughtco.ordereditor::default.edit_time_date',
+				'label' => 'lang:admin::lang.orders.label_order_date',
+				'type' => 'datepicker',
+				'disabled' => FALSE,
+				'mode' => 'date',
+				'span' => 'right',
+				'cssClass' => 'flex-width',
+			];
+			
+			$form->tabs['fields']['comment'] = [
+				'tab' => 'lang:thoughtco.ordereditor::default.edit_time_date',
+				'label' => 'lang:admin::lang.orders.label_comment',
+				'type' => 'textarea',
+				'disabled' => FALSE,
+			];	
+				$form->tabs['fields']['notes'] = [
+					'tab' => 'lang:thoughtco.ordereditor::default.notes',
+					'label' => 'lang:thoughtco.ordereditor::default.order_notes',
+					'span' => 'left',
+					'type' => 'textarea',
+				];
+				
+				
+			     Event::listen('admin.toolbar.extendButtonsBefore', function (Toolbar &$toolbar) {
+            if ($toolbar->getController() instanceof \Admin\Controllers\Orders)
+			    $toolbar->buttons['save']['context'][] = 'edit';
+				});						
+	        }
+        });
+    }
+}
+
+?>
---
+++ language/en/default.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'notes' => 'Notes',
+    'edit_time_date' => 'Edit time/date',
+    'order_notes' => 'Internal notes',
+];
